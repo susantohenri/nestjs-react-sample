5a96ea740eca75480410b570e0072f022be12e1e
Henri Susanto <henry.dinus@gmail.com>
2025-01-03 07:06:31 +0700

    shadcn: fix error from installation

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
   /*server: {
     proxy: {
       '/': {
@@ -12,4 +14,12 @@ export default defineConfig({
       }
     }
   }*/
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcss,
+          autoprefixer,
+        ],
+      },
+    },
 });
